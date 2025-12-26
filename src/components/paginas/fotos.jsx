9d6a3531e90a674bd318f9d829a225ee43c13a8e
Franilover <franilover@proton.me>
2025-12-26 05:22:55 -0300

    asdasd

--- src/components/paginas/fotos.jsx
+++ src/components/paginas/fotos.jsx
@@ -12,13 +12,13 @@ const Diario = () => {
     { fecha: "20 de Enero 2025", img: "/fotos/2025-01-20_1.jpg" },
     { fecha: "19 de Nov. 2024", img: "/fotos/2024-11-19_1.jpg" },
     { fecha: "13 de Nov. 2024", img: "/fotos/2024-11-13_1.jpg" },
-    { fecha: "11 de Nov. 2024", img: "/fotos/2024-11-11_2.jpg", desc: "EL MEJOR CONCIERTO!!!" },
-    { fecha: "11 de Nov. 2024", img: "/fotos/2024-11-11_1.jpg", desc: "EL MEJOR CONCIERTO!!!" },
+    { fecha: "11 de Nov. 2024", img: "/fotos/2024-11-11_2.jpg"},
+    { fecha: "11 de Nov. 2024", img: "/fotos/2024-11-11_1.jpg"},
     { fecha: "28 de Oct. 2024", img: "/fotos/2024-10-28_1.jpg",},
     { fecha: "10 de Oct. 2024", img: "/fotos/2024-10-05_2.jpg",},
     { fecha: "21 de Sept. 2024", img: "/fotos/2024-09-21_1.jpg"},
     { fecha: "16 de Ago. 2024", img: "/fotos/2024-08-16_1.jpg"},
-    { fecha: "27 de Jul. 2024", img: "/fotos/2024-07-27_1.jpg", desc: "Ensayo PAES con la Yoa :P" },
+    { fecha: "27 de Jul. 2024", img: "/fotos/2024-07-27_1.jpg"},
   ];
 
   return (
