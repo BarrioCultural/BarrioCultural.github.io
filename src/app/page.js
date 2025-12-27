da05716b20c0cbe580830b1fc0564b70482527ce
Franilover <franilover@proton.me>
2025-12-27 06:13:16 -0300

    asdasd

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,7 @@ export default function Home() {
   return (
     <div className="py-10 flex flex-col items-center justify-center">
       <div className="rounded-2xl bg-stone-900 p-8 shadow-2xl border border-stone-800">
-        <h1 className="text-3xl font-bold text-stone-100"> Test n.5 de tailwind</h1>
-        <p className="mt-2 text-stone-400">PADJKASDJ.</p>
+        <h1 className="text-3xl font-bold text-stone-100"> Barrio Cultura</h1>
       </div>
     </div>
   );
