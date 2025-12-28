6df044467cdb117fbb32f39a00090c1280407c76
Franilover <franilover@proton.me>
2025-12-28 13:59:21 -0300

    werertytu

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ export default function Home() {
     <div className="py-10 flex flex-col items-center justify-center">
       {/* Tu código de prueba de Tailwind o tu diseño de Nexus */}
       <div className="rounded-2xl bg-stone-900 p-8 shadow-2xl border border-stone-800">
-        <h1 className="text-3xl font-bold text-stone-100"> Test n.5 de tailwind</h1>
+        <h1 className="text-5xl font-bold text-center text-primary"> Nuevo dibujo navideño!!</h1>
         <p className="mt-2 text-stone-400">PADJKASDJ.</p>
       </div>
     </div>
