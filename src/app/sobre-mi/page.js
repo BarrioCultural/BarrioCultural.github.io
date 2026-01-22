d111ab0d028b32f60e9648e9aeb1e7b8ac9a0fe2
Franilover <franilover@proton.me>
2026-01-21 22:23:17 -0300

    aaa

--- src/app/sobre-mi/page.js
+++ src/app/sobre-mi/page.js
@@ -3,7 +3,7 @@ import SobreMi from '@/components/paginas/sobre-mi';
 
 export default function Page() {
   return (
-    <main className="min-h-screen pt-32 pb-20 px-4 flex justify-center bg-bg-main">
+    <main className="min-h-screen pt-32 pb-20 px-4 flex justify-center">
       <SobreMi />
     </main>
   );
