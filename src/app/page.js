08dc89ae882183b086e7a2b94334de073bb861c5
Franilover <franilover@proton.me>
2026-01-21 22:27:28 -0300

    a

--- src/app/page.js
+++ src/app/page.js
@@ -9,7 +9,7 @@ export default function Page() {
       initial={{ opacity: 0, y: 10 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.5 }}
-      className="min-h-screen pt-32 pb-20 px-4 flex justify-center "
+      className="min-h-screen pt-32 pb-20 px-4 flex justify-center bg-[#F0F0F0]"
     >
       <SobreMi />
     </motion.main>
